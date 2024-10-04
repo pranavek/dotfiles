54f4856d1ffa0654c0ca0fbaad3f2caf62edceea
Pranav <pranavek@users.noreply.github.com>
2024-10-04 13:56:37 -0500

    Update alias.fish

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -1,6 +1,16 @@
 # Replace cat with bat https://github.com/sharkdp/bat
 alias cat=bat
 
+ # Misc
+alias vi=nvim
+alias v=vi
+alias c=clear
+alias g=git
+alias ls='ls --color=auto'
+alias ll='ls -lh --color=auto'
+alias ..='cd ..'
+alias ...='cd ../../'
+
 # Git
 alias gru="git remote get-url --all origin"
 alias gcb='git branch | grep -v $(git branch --show-current) | fzf | xargs git checkout'
@@ -8,5 +18,9 @@ alias gpl='git pull origin $(git branch --show-current)'
 alias gph='git push origin HEAD'
 alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias gst="git status"
+alias gacp='echo "Enter commit message : " && read MSG && git add . && git commit -m "$MSG" && git push origin HEAD
+
+alias sudop='sudo -E env "PATH=$PATH"'
+
 
 
