29e1ffe7613feb9a79380e285baf0939824ecc55
Pranav <pranavek@users.noreply.github.com>
2025-07-16 14:07:06 -0500

    Update alias.fish

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -14,7 +14,7 @@ alias cdf='cd $(find * -type d | fzf)'
 
 # Git
 alias gru="git remote get-url --all origin"
-alias gcb='git branch | grep -v $(git branch --show-current) | fzf | xargs git checkout'
+alias gcb='git branch | grep -v $(git branch --show-current) | fzf --header Checkout | xargs git checkout'
 alias gpl='git pull origin $(git branch --show-current)'
 alias gph='git push origin HEAD'
 alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
